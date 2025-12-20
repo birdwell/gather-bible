6d7d0d5326c8a3d8da6a2aa3a44d7a8426d9f824
Josh Birdwell <jbirdwell34@gmail.com>
2025-12-20 07:35:12 -0600

    Initial Commit

---
+++ Gather BibleTests/Gather_BibleTests.swift
@@ -0,0 +1,17 @@
+//
+//  Gather_BibleTests.swift
+//  Gather BibleTests
+//
+//  Created by Josh Birdwell on 12/20/25.
+//
+
+import Testing
+@testable import Gather_Bible
+
+struct Gather_BibleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
